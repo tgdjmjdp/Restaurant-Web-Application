13ba0c87840fa2fb7d929044b18cba1fb5635bb0
tgdjmjdp <tgdjmjdp2@gmail.com>
2019-09-17 21:35:43 +0700

    check point

--- client/src/components/Main/Home/Home.js
+++ client/src/components/Main/Home/Home.js
@@ -4,6 +4,16 @@ import Typography from '@material-ui/core/Typography';
 import clsx from 'clsx';
 import { useStyles } from '../../../Styles/Navbar'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import {
+    MDBDropdown,
+    MDBDropdownItem,
+    MDBDropdownMenu,
+    MDBDropdownToggle,
+    MDBBtn,
+    MDBIcon
+} from 'mdbreact'
+
 
 const Home = ({
     navState: {
@@ -20,6 +30,34 @@ const Home = ({
                     [classes.contentShift]: sidebarOpen,
                 })} >
                 <div  >
+
+                    <MDBDropdown>
+                        <MDBDropdownToggle
+                            className=""
+                            nav
+                            color="primary"
+                        >
+                            <div className=" p-1" >
+                                <MDBIcon size="2x" icon="bell" />
+                            </div>
+                        </MDBDropdownToggle>
+                        <MDBDropdownMenu basic>
+                            <MDBDropdownItem>
+                                <span style={{ fontFamily: "Saysettha OT" }} className="">
+                                    ແຈ້ງເຕືອນ
+                                                        </span>
+                            </MDBDropdownItem>
+                            <MDBDropdownItem divider />
+                            <MDBDropdownItem
+                                style={{ fontFamily: "Saysettha OT" }}
+                                className="align-middle"
+                            >
+                                <Link to="/restaurant/create" >
+                                    ເພີ່ມຮ້ານອາຫານ
+                                                        </Link>
+                            </MDBDropdownItem>
+                        </MDBDropdownMenu>
+                    </MDBDropdown>
                     <Typography paragraph>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
                         ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
